529152fa9306a02f60c582f92aaf2c6ca0170139
firas najjar <firasngr@gmail.com>
2025-07-25 21:22:50 +0300

    final test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,27 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Test", menuName = "Scriptable Objects/Test")]
 public class Test : ScriptableObject
 {
-    public string testString;
-    public int testInt;
-    public float testFloat;
-    public bool testBool;
-    public Color testColor;
-    public Vector3 testVector3;
-    public Quaternion testQuaternion;
-    public GameObject testGameObject;
-    public Transform testTransform;
-    public Rigidbody testRigidbody;
-    public Collider testCollider;
-    public MeshRenderer testMeshRenderer;
-    public MeshFilter testMeshFilter;
-    public MeshCollider testMeshCollider;
     public Material testMaterial;
     public Texture2D testTexture2D;
     public AudioClip testAudioClip;
     public AnimationCurve testAnimationCurve;
     public Gradient testGradient;
-    public List<string> testList;
-    public Dictionary<string, int> testDictionary;
-    public HashSet<string> testHashSet;
-  Debug.Log("Test");
+
+    public void Test()
+    {
+        Debug.Log("Test");
+    }
 }
